3607668b3ae24d5dc1cc9ac8d4158072c063c48c
ebatank <battawar.ankit@gmail.com>
2016-09-27 07:35:15 -0700

    Update header.php

--- header.php
+++ header.php
@@ -40,8 +40,8 @@
         </div>
         <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1" >
           <ul class="nav navbar-nav">
-			<li><a href="http://www.thesoftwareguy.in/php-tutorial/">PHP TutorialS-</a></li>
-			<li><a href="http://www.thesoftwareguy.in/working-demos/">More DemoS-</a></li>
+			<li><a href="http://www.reancloud.com/company/">About REAN</a></li>
+			<li><a href="http://www.reancloud.com/cloud-offerings/">REAN Offerings</a></li>
             <li><a href="http://www.thesoftwareguy.in/mini-project/">More Projects</a></li>
           </ul>
         </div>
